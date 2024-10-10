8d3dc2f864b61e5f6ebbcc572ec23f4b16a7b104
Igor Sokolovic <igor.sokolovic01@gmail.com>
2024-10-10 23:12:00 +0200

    First Step 3, renaming the variables

--- homework6/main.cxx
+++ homework6/main.cxx
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <string>
-int main(int var0, char *var1[]) {
-  if (var0 == 3) {
-    std::string var2{var1[0]};
-    auto var3{*(var1[1])};
-    auto var4{var2.size()};
-    auto var5{std::atoi(var1[2])};
-    auto var6{0};
-    auto var7{0};
-    std::string var8{var1[1]};
+
+int main(int argument_count, char *arguments[]) {
+    
+  bool has_required_arguments = (argument_count == 3);
+
+  if (has_required_arguments) {
+    std::string program_name{arguments[0]};
+    char first_char_of_argument{*(arguments[1])};
+    std::size_t program_name_length{program_name.size()};
+    int provided_checksum{std::atoi(arguments[2])};
+
+    int calculated_value{0};
+    std::string input_string{arguments[1]};
+    int index{0};
+
     while (true) {
-      var6 += var8[var7++];
-      if (var7 >= static_cast<int>(var8.size())) {
+      calculated_value += input_string[index++];
+      if (index >= static_cast<int>(input_string.size())) {
         break;
       }
     }
-    if ((var6 ^ var3 * 3) << (var4 & 0x1f) == var5) {
+
+    if ((calculated_value ^ first_char_of_argument * 3) << (program_name_length & 0x1f) == provided_checksum) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
     }
   }
 }
+
