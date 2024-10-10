78246f08b11de4dfc328ee098d1203e853348076
Igor Sokolovic <igor.sokolovic01@gmail.com>
2024-10-10 23:20:01 +0200

    Second Step 3, facoring out the while loop

--- homework6/main.cxx
+++ homework6/main.cxx
@@ -1,27 +1,32 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+
+
+int compute_checksum(const std::string& input_string) {
+    int checksum = 0;
+    for (char ch : input_string) {
+        checksum += ch;
+    }
+    return checksum;
+}
 
 int main(int argument_count, char *arguments[]) {
     
   bool has_required_arguments = (argument_count == 3);
 
   if (has_required_arguments) {
-    std::string program_name{arguments[0]};
+    std::string program_name{arguments[0]}; 
     char first_char_of_argument{*(arguments[1])};
     std::size_t program_name_length{program_name.size()};
     int provided_checksum{std::atoi(arguments[2])};
 
-    int calculated_value{0};
     std::string input_string{arguments[1]};
-    int index{0};
 
-    while (true) {
-      calculated_value += input_string[index++];
-      if (index >= static_cast<int>(input_string.size())) {
-        break;
-      }
-    }
+   
+    int calculated_value = compute_checksum(input_string);
 
+    
     if ((calculated_value ^ first_char_of_argument * 3) << (program_name_length & 0x1f) == provided_checksum) {
       std::cout << "Correct!" << std::endl;
     } else {
@@ -29,4 +34,5 @@ int main(int argument_count, char *arguments[]) {
     }
   }
 }
+
 
